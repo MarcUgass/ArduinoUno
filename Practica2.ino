c7ad5edfd717d03922a81e1d92f22e86c0cc23c4
MarcUgass <93116190+MarcUgass@users.noreply.github.com>
2023-11-28 12:20:38 +0100

    Add files via upload

--- Practica2.ino
+++ Practica2.ino
@@ -7,72 +7,120 @@
 SoftwareSerial mySerial(2, 3);
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 
+String userName = "";
+uint8_t id = 0;
+
 void setup() {
-   Serial.begin(9600);
-  while (!Serial);
-  Serial.println("Adafruit Finger Print Sensor Test");
+  Serial.begin(9600);
+  //while (!Serial);
+  Serial.println("Inicio de programa");
+
+  finger.begin(57600);
 
   if (finger.verifyPassword()) {
     Serial.println("Sensor de huella dactilar encontrado!");
   } else {
-    Serial.println("Verificación de contraseña incorrecta");
+    Serial.println("Sensor de huella dactilar no encontrado.");
+    while(1);
   }
 
 }
 
 void loop() {
   Serial.println("Elija una opción:");
-  Serial.println("1 - Alta de usuario");
-  Serial.println("2 - Identificación de usuario");
+  Serial.println("1 - Registro de usuario");
+  Serial.println("2 - Reconocimiento de usuario");
 
   while (!Serial.available());
 
-  char choice = Serial.read();
+  char opcion = Serial.read();
 
-  if (choice == '1') {
-    enrollUser();
-  } else if (choice == '2') {
-    identifyUser();
+  if (opcion == '1') {
+    registro();
+  } else if (opcion == '2') {
+    reconocimiento();
   } else {
     Serial.println("Opción no válida");
   }
 }
 
-void enrollUser() {
+void registro() {
   Serial.println("Ingrese el nombre y apellidos del usuario:");
 
   while (!Serial.available());
 
-  String userName = Serial.readString();
+  userName = Serial.readString();
   userName.trim();
 
-  Serial.println("Coloque el dedo en el sensor...");
-
-  uint8_t id = 0;
-
-  while (!finger.getImage());
-  finger.image2Tz();
-  id = finger.storeModel(0);
-
-  if (id != FINGERPRINT_OK) {
-    Serial.println("Error al almacenar la huella dactilar");
-  } else {
-    Serial.println("Huella dactilar almacenada exitosamente");
+  int p = -1;
+  Serial.print("Esperando validar usuario con el id: #"); Serial.println(id);
+  while (p != FINGERPRINT_OK) {
+    p = finger.getImage();
+    switch (p) {
+    case FINGERPRINT_OK:
+      break;
+    case FINGERPRINT_NOFINGER:
+      break;
+    default:
+      Serial.println("Error");
+      break;
+    }
   }
+
+    p = finger.image2Tz(1);
+    switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Usuario registrado!");
+      break;
+    default:
+      Serial.println("Error");
+      return p;
+  }
+  p = finger.storeModel(id);
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Guardado!");
+  } else {
+    Serial.println("Error");
+    return p;
+  }
+  id +=1;
+  
 }
 
-void identifyUser() {
+void reconocimiento() {
   Serial.println("Coloque el dedo en el sensor...");
-
-  while (!finger.getImage());
-  finger.image2Tz();
-
-  int id = finger.fingerFastSearch();
-
-  if (id == FINGERPRINT_OK) {
-    Serial.println("Usuario reconocido");
-    Serial.println("Bienvenido!");
-  } else {
-    Serial.println("Acceso no autorizado");
+  delay(2000);
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("Huella dactilar no encontrada.");
+      return p;
+    default:
+      Serial.println("Error");
+      return p;
   }
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      break;
+    default:
+      Serial.println("Error");
+      return p;
+  }
+
+  p = finger.fingerSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Se ha encontrado una coincidencia!");
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("No se ha encontrado una coincidencia!");
+    return p;
+  } else {
+    Serial.println("Error");
+    return p;
+  }
+
+  Serial.println("Bienvenido " + userName +"!"); //Serial.print(finger.fingerID);
 }
